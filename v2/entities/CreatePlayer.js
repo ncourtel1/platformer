2169580aaa9ed84d8374d47ba4d4983265ae6dfe
ncourtel <courtel.nattan19@gmail.com>
2024-12-17 13:53:08 +0100

    entities update

--- v2/entities/CreatePlayer.js
+++ v2/entities/CreatePlayer.js
@@ -1,14 +1,14 @@
-import { Entity } from "./Rigidbody"
-import { InputComponemt } from "../components/InputComposent"
-import MovementComponent from "../components/MovementComponent"
-import { StateComponent } from "../components/StateComponent"
-import { PhysicsComponent } from "../components/PhysicsComponent"
+import { Entity } from "./Rigidbody.js"
+import { InputComponemt } from "../components/InputComponent.js"
+import { StateComponent } from "../components/StateComponent.js"
+import { PhysicsComponent } from "../components/PhysicsComponent.js"
+import { PlayerData } from "../components/PlayerData.js"
 
 export default function CreatePlayer(x, y){
    const player = new Entity();
    player.addComponent('input', new InputComponemt());
-   player.addComponent('physics', new PhysicsComponent());
-   player.addComponent('movement', MovementComponent);
+   player.addComponent('physics', new PhysicsComponent(x, y));
+   player.addComponent('data', PlayerData);
    player.addComponent('state', new StateComponent());
    return player
 }
