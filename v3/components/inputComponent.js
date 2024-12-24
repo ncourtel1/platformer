87bb1700b118043a03be135d69d08543ea2529f8
ncourtel <courtel.nattan19@gmail.com>
2024-12-24 14:10:47 +0100

    update for jump and gravity system

--- v3/components/inputComponent.js
+++ v3/components/inputComponent.js
@@ -7,15 +7,28 @@ export default class InputComponent{
 
       window.addEventListener("keydown", (e) =>{
          this.keys.add(e.key);
+         console.log(e.key)
       })
       window.addEventListener("keyup", (e) =>{
          this.keys.delete(e.key);
       })
+
+      // window.addEventListener("keydown", (e)=>{
+      //    if(e.key === "a" && e.key === "d") this.x = 0;
+      //    else if(e.key === "a") this.x = -1;
+      //    else if(e.key === "d") this.x =1;
+      //    if(e.key === " ") this.jumpPressed = true;
+      // })
+      // window.addEventListener("keyup", (e)=>{
+      //    if(e.key === "a" || e.key === "d") this.x = 0;
+      //    if(e.key === " ") this.jumpPressed = false;
+      // })
    }
 
    update(){
       this.x = 0;
       this.y = 0;
+      this.jumpPressed = false;
       if(this.keys.has("d") && this.keys.has("a")) this.x = 0;
       else if(this.keys.has("d")) this.x = 1;
       else if(this.keys.has("a")) this.x = -1;
--- v3/components/playerDataComponent.js
+++ v3/components/playerDataComponent.js
@@ -1,8 +1,4 @@
 export const PlayerDataComponent = {
-   // Run
-   runAcceleration: 9.5,
-   runDecceleration: 9.5,
-   runMaxSpeed: 9.5,
-   runAccelAmount: (50 * 9.5) / 9.5,
-   runDeccelAmount: (50 * 9.5) / 9.5
+   gravity: 10,
+   jumpForce: 200
 }
