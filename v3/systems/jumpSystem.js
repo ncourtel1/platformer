c10811425cdf3ba5df1d91bbdf2be210cc5d5e3b
ncourtel <courtel.nattan19@gmail.com>
2024-12-24 14:10:12 +0100

    gravity and jump system

--- v3/systems/jumpSystem.js
+++ v3/systems/jumpSystem.js
@@ -4,7 +4,16 @@ export default class JumpSystem{
          const position = entity.getComponent('position');
          const input = entity.getComponent('input');
          const velocity = entity.getComponent('velocity');
-         const playerData = entity.getComponent('data');
+         const data = entity.getComponent('data');
+         const state = entity.getComponent('state');
+
+         if(position && input && state.isGrounded){
+            input.update()
+            if(input.jumpPressed){
+               position.y -= data.jumpForce;
+               input.jumpPressed = false;
+            } 
+         }
       }
    }
 }
